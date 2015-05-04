209c5a7da2787837d2b906b174d3a18eda0129b3
Gonzalo Solera <gonzalo1796@gmail.com>
2015-05-04 20:10:01 +0200

    Especificacion main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+/** @file main.cpp
+    @brief Programa principal del gestor de tareas.
+
+    Crea un objeto Agenda, al que le hace leer una nueva Comanda y la ejecuta hasta que la comanda sea salir.
+ */
+
 #include <iostream>
 #include "agenda.h"
 using namespace std;
+
 
 int main() {
     Agenda agenda;
