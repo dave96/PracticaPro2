2af7a4872a381adc2ac8db6df361f3f800b71ab9
David <davidalro@gmail.com>
2015-05-08 17:12:48 +0200

    Comienzo de  implementación de menú

--- menu.h
+++ menu.h
@@ -7,12 +7,25 @@
 #include "tarea.h"
 using namespace std;
 
+/** @class Menu
+
+
 class Menu {
-    vector<map<Fecha, Tarea, less<Fecha> >::iterator> iteradores;
+    vector< map<Fecha, Tarea, less<Fecha> >::iterator > iteradores;
 
 public:
+    \pre Cierto.
     Menu();
     ~Menu();
+
+    /* Modificadoras */
+    \pre Iterador it apunta a la tarea en el map.
+    void anadirTarea (map<Fecha, Tarea, less<Fecha>>::iterator& it);
+
+    \pre Int num <= numTareas.
+    \post Eliminada la tarea numero num. */
+    void eliminarTarea (const int& num);
+
 };
 
 #endif // MENU_H
