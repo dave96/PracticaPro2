e995e330548a682a31074cb05284ff699fad62a7
David <davidalro@gmail.com>
2015-05-12 13:31:19 +0200

    Arreglos

--- menu.cpp
+++ menu.cpp
@@ -20,15 +20,15 @@ void Menu::anadirTarea(map<Fecha, Tarea, less<Fecha> >::iterator& it) {
 
 void Menu::eliminarTarea (const int& num, bool& error) {
   error = false;
-  if(num <= iterator.size()) iterator[num-1] = NULL;
+  if(num <= iteradores.size()) iteradores[num-1] = NULL;
   else error = true;
 }
 
 map<Fecha, Tarea, less<Fecha> >::iterator Menu::consultarTarea(const int& num, bool& error) {
-  if(num <= iterator.size()) {
-    if(iterator[num-1] != NULL) {
+  if(num <= iteradores.size()) {
+    if(iteradores[num-1] != NULL) {
       error = false;
-      return iterator[num-1];
+      return iteradores[num-1];
     } else {
       error = true;
       return NULL;
