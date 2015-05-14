3bb7480dcec66b57ebfbd658d3bc6f545451827f
dave96 <dave96@dtecno.com>
2015-05-14 12:21:01 +0200

    Especificacion menu

--- menu.h
+++ menu.h
@@ -8,6 +8,15 @@
 using namespace std;
 
 
+/** @class Menu
+    @brief Gestiona el menú de la agenda.
+
+    Es una estructura de datos que permite a la agenda saber que número está asociado con que tarea en todo momento.
+    Se usa en las comandas de modificación y eliminación, y crea en las consultas.
+    Los métodos de acceso público sirven para añadir y consultar tareas, porque el objeto se borra a cada consulta.
+*/
+
+
 
 class Menu {
 private:
