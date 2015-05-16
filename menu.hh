8554e7119d8c83f77e135876eb66716532bf2cda
David <davidalro@gmail.com>
2015-05-16 03:02:15 +0200

    Documentacion menu

--- menu.hh
+++ menu.hh
@@ -31,18 +31,30 @@ private:
 
 
 public:
-    //pre Cierto.
+    /** @brief Crea un menú vacío.
+    \pre Cierto.
+    \post  Menú vacío creado.*/
     Menu();
     ~Menu();
 
-    /* Modificadoras */
-    //pre Iterador it apunta a la tarea en el map.
+    /** @brief Añade una tarea al menú actual.
+    \pre Iterator válido apuntando a la tarea.
+    \post Tarea añadida al p.i. */
     void anadirTarea (map<Fecha, Tarea, less<Fecha> >::iterator& it);
 
+    /** @brief Marca como no válida una tarea del menú, para que no se pueda acceder a ella.
+    \pre Número de la tarea en el menú.
+    \post  Tarea inhabilitada (el iterator no se puede recuperar) */
     void eliminarTarea (const int& num);
 
+    /** @brief Cambia el iterador que apunta a la tarea num.
+    \pre iterador it válido, número de tarea en el menú.
+    \post  La tarea numero num ahora apunta al iterator it */
     void modificarTarea(const int& num, map<Fecha, Tarea, less<Fecha> >:: iterator& it);
 
+    /** @brief Devuelve el iterador de la tarea número num del menú.
+    \pre Número de tarea en el menú, error falso.
+    \post  Devuelve el iterador a la tarea y error falso si hay es correcta la consulta, y error verdadero y un iterador vacío si no es correcta. */
     map<Fecha, Tarea, less<Fecha> >::iterator consultarTarea (const int& num, bool& error) const;
 
 };
